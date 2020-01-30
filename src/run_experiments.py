bada1633772d95838d1a96aa9aad6d3f9eb50171
MatheusWoeffel <matheuswoeffelcamargo@hotmail.com>
2020-01-30 15:12:15 -0300

    Fixed doe generation bug + command line arguments

--- src/run_experiments.py
+++ src/run_experiments.py
@@ -1,5 +1,6 @@
 import os
-from doe import *
+import sys
+from doe import generateDoeCSV
 from ai_benchmark import AIBenchmark
 
 
@@ -32,20 +33,22 @@ def change_thread_mapping(thread_mapping_policy):
 
 
 if __name__ == "__main__":
-<<<<<<< HEAD
-    number_of_rounds = 2
-    machine_name = "hype1"
-    benchmark = AIBenchmark(use_CPU=True)
-=======
-    number_of_rounds = 3
+    """ python3 run_experiments.py machine_name start_index n_rounds
+    """ 
+    path = "pathofexperiment here"
+    machine_name = sys.argv[1]
+    start_index = int(sys.argv[2])
+    n_rounds = int(sys.argv[3])
 
->>>>>>> b243f7e79d81cde401ddd6070c9d3c131deeca28
-    for i in range(0,number_of_rounds):
+    benchmark = AIBenchmark(use_CPU=True)
+    
+    #Generate a doe corresponding to some round and them reads it to pick up the mappings corresponding to the round
+    for i in range(start_index, start_index + n_rounds):
         generateDoeCSV("doe_"+ machine_name + "_{number}.csv".format(number=i))
-        #opens the default doe csv
+        
         with open("doe_" + machine_name + "_{number}.csv".format(number=i), "r") as doe:
+            print("------" + i + " round------", flush=True)
             experiment_rounds = doe.readlines()
-            list_of_results = []
 
             for experiment_round in experiment_rounds:
                 mappings = experiment_round.split(",")
@@ -53,6 +56,7 @@ if __name__ == "__main__":
                 data_mapping =   mappings[1]
                 change_data_mapping(data_mapping)
                 change_thread_mapping(thread_mapping)
-                current_result = benchmark.run()
-            doe.close()
+                current_result = benchmark.run_training()
+                sys.stdout.flush()
+            
 
